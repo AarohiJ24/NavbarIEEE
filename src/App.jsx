6cc01316b52683e7cb497a57d276e93f349eca3b
AarohiJ24 <aarohij248@gmail.com>
2025-07-08 19:24:34 +0530

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Navbar2 from './Navbar2'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar2/>
+    </>
+  );
+}
+
+export default App
